54a3752dc620b8d22364445eae73f7b81cb0b291
seongtaekkim <chxortnl@gmail.com>
2023-03-23 14:54:36 +0900

    fix: unit test logic

--- test/test_unit.hpp
+++ test/test_unit.hpp
@@ -1,25 +1,14 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   test_unit.hpp                                      :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ecaceres <ecaceres@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/25 23:18:42 by ecaceres          #+#    #+#             */
-/*   Updated: 2020/12/25 23:18:42 by ecaceres         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#ifndef TEST_UNIT_HPP
+# define TEST_UNIT_HPP
 
-#ifndef TEST_UNIT_HPP_
-# define TEST_UNIT_HPP_
-
-#include "../srcs/log/LogFactory.hpp"
+// #include "../srcs/log/LogFactory.hpp"
+#include <iostream>
 
 # ifndef RUN_TESTS
-#  define RUN_TESTS 0
+#  define RUN_TESTS 1
 # endif
 
-// #include <tests/case/TestCase.hpp>
+#include "case/TestCase.hpp"
 #include <list>
 #include <map>
 #include <string>
@@ -46,12 +35,12 @@
 	static int __attribute__((unused)) CASE_METHOD_NAME(__attribute__((unused)) int argc, __attribute__((unused)) char **argv, __attribute__((unused)) char **envp)
 # endif
 
-static Logger TEST_CASE_LOG = LoggerFactory::get("test-case");
+// static Logger TEST_CASE_LOG = LoggerFactory::get("test-case");
 
 # define ASSERT(cond) \
 	if (!(cond)) \
 	{ \
-		TEST_CASE_LOG.fatal() << "ASSERT() " << __FILE__ << ":" << __LINE__ << " -- " << #cond << std::endl; \
+		std::cerr << "ASSERT() " << __FILE__ << ":" << __LINE__ << " -- " << #cond << std::endl; \
 		return (1); \
 	}
 
@@ -59,7 +48,7 @@ static Logger TEST_CASE_LOG = LoggerFactory::get("test-case");
 	try \
 	{ \
 		code; \
-		TEST_CASE_LOG.fatal() << "ASSERT_EXCEPT() " << __FILE__ << ":" << __LINE__ << " -- " << #code << std::endl; \
+		std::cerr << "ASSERT_EXCEPT() " << __FILE__ << ":" << __LINE__ << " -- " << #code << std::endl; \
 		return (1); \
 	} \
 	catch (...) \
@@ -73,7 +62,7 @@ static Logger TEST_CASE_LOG = LoggerFactory::get("test-case");
 	} \
 	catch (std::exception &exception) \
 	{ \
-		TEST_CASE_LOG.fatal() << "ASSERT_NO_EXCEPT() " << __FILE__ << ":" << __LINE__ << " -- " << #code << " -- " << exception.what() << std::endl; \
+		std::cerr << "ASSERT_NO_EXCEPT() " << __FILE__ << ":" << __LINE__ << " -- " << #code << " -- " << exception.what() << std::endl;  \
 		return (1); \
 	}
 
@@ -82,4 +71,4 @@ extern std::map<std::string, std::list<TestCase> > g_test_units_and_cases;
 int
 test_main(int argc, char **argv, char **envp);
 
-#endif /* TEST_UNIT_HPP_ */
+#endif
